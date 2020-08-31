489a8511b0ec71410961bb85c3d9d68aadc17225
jokr91 <hannes.kraml@gmail.com>
2020-08-31 15:19:32 +0200

    Lots of refactoring
    Refactored many parts of the GIST setup and initialization, also started
    with refactoring of the DoAction function. This function is still MUCH
    too long and will be made smaller.
    Also changed a few things, I removed the min and max pointers, as these
    were not used anyway.
    The refactored functions must still be documented in the header file, as
    well as will be moved to the end of the cpp file. This allows a better
    understanding of the code and what happens at which point in the
    calculation.

--- cuda_kernel_gist/GistCudaSetup.cuh
+++ cuda_kernel_gist/GistCudaSetup.cuh
@@ -16,15 +16,20 @@ public:
   }
 };
 
+struct EnergyReturn {
+  std::vector<float> eww;
+  std::vector<float> esw;
+};
+
 // Function definitions
 void allocateCuda(void **, int);
 void copyMemoryToDevice(void *, void *, int);
 void copyMemoryToDeviceStruct(float *, int *, bool *, int *, int, void **, float *, float *, int, void **);
 void freeCuda(void *);
-std::vector<std::vector<float> > doActionCudaEnergy(const double *, int *, int , void *, void *,
-                            int , float *, float *, int , float *, 
-                            float *, int, float, int *, int *, float *, float *,
-                            int *, int *, bool);
+EnergyReturn doActionCudaEnergy(const double *coords, int *NBindex_c, int ntypes, void *parameter, void *molecule_c,
+                            int boxinfo, float *recip_o_box, float *ucell, int maxAtoms, int headAtomType, 
+                            float neighbourCut2, int *result_o, int *result_n, float *result_w_c, float *result_s_c,
+                            int *result_O_c, int *result_N_c, bool doorder);
 std::vector<std::vector<float> > doActionCudaEntropy(std::vector<std::vector<Vec3> >, int, int, int, std::vector<std::vector<Quaternion<float> > >, float, float, int);
 
 #endif
